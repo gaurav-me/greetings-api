3213f4ca9998ed506b10ad5f9edfad42f19265c3
gs195 <timedilation01@gmail.com>
2019-06-25 17:25:50 +0800

    17th commit added test and server.js

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("./index.js");
+
+describe("My service", () => {
+  it("Should get a greeting", () => {
+    return request(app)
+      .get("/")
+      .expect("Hello World! This app is live!");
+  });
+});
